522a2567102bf32eff9ac9d8947cfd2e3c29d032
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-02-10 20:59:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function makeid(l) {
   // write your code here
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let Randomstring = "";
+	for(let i=0;i<l;i++){
+		let randomIndex = Math.floor(Math.random() * characters.length);
+		Randomstring+= characters.charAt(randomIndex);
+	}
+	return Randomstring;
 }
 
-// Do not change the code below.
+// Do not change the code below
 const l = prompt("Enter a number.");
 alert(makeid(l));
